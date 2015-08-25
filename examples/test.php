df7b67aac36ffb655ac1599f2c88acada9c5e5b2
sotoz <topgan1@gmail.com>
2015-08-26 01:48:09 +0300

    Fixing group getAll(). Added more examples to demonstrate jasmin-web.

--- examples/test.php
+++ examples/test.php
@@ -5,30 +5,75 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require '../vendor/autoload.php';
+echo"START<br/>";
 
-/*
 $groupconn = new JasminGroup();
+$userconn = new JasminUser();
+$filterconn = new JasminFilter();
+
+echo "<h2>Group addition</h2>";
+$groupconn->set_id('customers1');
+if ($groupconn->save()){
+    echo 'Group customers1 was created<br/>';
+}else{
+    echo 'Group customers1 was NOT created<br/>';
+}
+
+$groupconn->set_id('customers2');
+if ($groupconn->save()){
+    echo 'Group customers2 was created<br/>';
+}else{
+    echo 'Group customers2 was NOT created<br/>';
+}
 
 
-$groupconn->id = 'customers3';
-$groupconn->save();
-
-$groupconn->id = 'customers2';
-$groupconn->save();
+echo "<h2>Group Listing</h2>";
+var_dump( $groupconn->getAll());
 
 
-$groupconn->id = 'customers2';
-$groupconn->delete();
-//$groupconn->getAll();
 
-*/
-$user = new JasminUser();
+//DELETION
+echo "<h2>Group Deletion</h2>";
+$groupconn->set_id('customers1');
+if ($groupconn->delete()){
+    echo 'Group customers1 was deleted<br/>';
+}else{
+    echo 'Group customers1 was NOT deleted<br/>';
+}
 
-$user->id = 'sotiris';
-$user->properties['username'] = 'sotiris';
-$user->properties['password'] = 'sotiris';
-$user->properties['gid'] = 'customers3';
-echo $user->save();
+
+$groupconn->set_id('customers2');
+if ($groupconn->delete()){
+    echo 'Group customers2 was deleted<br/>';
+}else{
+    echo 'Group customers2 was NOT deleted<br/>';
+}
+
+$groupconn->set_id('customers3');
+if ($groupconn->delete()){
+    echo 'Group customers3 was deleted<br/>';
+}else{
+    echo 'Group customers3 was NOT deleted<br/>';
+}
+
+$groupconn->set_id('test odiki');
+if ($groupconn->delete()){
+    echo 'Group test odiki was deleted<br/>';
+}else{
+    echo 'Group test odiki was NOT deleted<br/>';
+}
+
+echo "<h2>Group Listing</h2>";
+
+var_dump($groupconn->getAll());
+
+
+$userconn->set_id('sotiris');
+$userconn->properties['username'] = 'sotiris';
+$userconn->properties['password'] = 'sotiris';
+$userconn->properties['gid'] = 'customers3';
+echo $userconn->save();
+echo  "<br/>END";
 //$smppconn = new JasminSMPPClientConnector();
 
 //$smppconn->cid = 'test';
--- src/Jookies/JasminGroup.php
+++ src/Jookies/JasminGroup.php
@@ -21,4 +21,32 @@ class JasminGroup extends JasminObject
         $this->id = $id;
         $this->properties['gid'] = $id;
     }
+
+    public function getAll()
+    {
+        $fetch_groups = parent::getAll();
+
+        // Explode jcli command output to fetch groups
+        $exploded = explode("#", $fetch_groups);
+
+        // Unset first and second elements that include unwanted results from the command group -l
+        unset($exploded[0]);
+        unset($exploded[1]);
+
+        $groups = array();
+        foreach ($exploded as $expl) {
+            $group = trim($expl);
+
+            //fetch string before the "Total Groups:" lectic
+            $ff = strstr($expl, 'Total Groups:', true);
+            if (!empty($ff)) {
+                $group = trim($ff);
+            }
+
+            array_push($groups, $group);
+        }
+
+        return $groups;
+
+    }
 }
